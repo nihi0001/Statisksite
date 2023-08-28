e816375392ac4da1d584db3c984d395e26c9af49
Nina <nihi0001@kea.dk>
2023-08-28 14:16:12 +0200

    working

--- produktliste.js
+++ produktliste.js
@@ -1,3 +1,4 @@
+//fetche
 fetch("https://kea-alt-del.dk/t7/api/products?limit=100")
     .then(res => res.json())
     .then(showProducts)
@@ -9,13 +10,55 @@ function showProducts(products) {
 
 function showProduct(product) {
     //console.log(product);
-    // fang template
-    const template = document.querySelector("#smallProductTemplate").textContent;
-    // lav en kopi
+    // clone, ændre og appende
+    const template = document.querySelector("#smallProductTemplate").content;
     const copy = template.cloneNode(true);
 
-    //appende
-    document.querySelector("main").appendChild(copy);
+    // ændre indhold
+    copy.querySelector("p.id").textContent = product.id;
+    copy.querySelector("p.gender").textContent = product.gender;
+    copy.querySelector("p.category").textContent = product.category;
+    copy.querySelector("p.subcategory").textContent = product.subcategory;
+    copy.querySelector("p.articletype").textContent = product.articletype;
+    copy.querySelector("p.productionyear").textContent = product.productionyear;  
+    copy.querySelector("p.productdisplayname").textContent = product.productdisplayname;
+    copy.querySelector("p.price").textContent = product.price;
+    copy.querySelector("p.discount").textContent = product.discount;
+    copy.querySelector("p.brandname").textContent = product.brandname;
+    copy.querySelector("p.soldout").textContent = product.soldout;
 
+    const parent = document.querySelector(".grid_1-1-1-1");
+
+
+   // if(product.soldout){
+        //produktet er udsolgt
+       // copy.querySelector("article").classList.add("soldOut");
+   // }
+    //appende
+   // document.querySelector("main").appendChild(copy);
+
+
+
+parent.appendChild(copy);
 }
 
+// html:
+// <template id="smallProductTemplate">
+// <article class="smallProduct">
+//     <a href="produkt.html">
+//         <img src="https://kea-alt-del.dk/t7/images/webp/640/1164.webp" alt="">
+//     </a>
+//     <p class="id">1163 </p>
+//     <p class="gender">Men</p>
+//     <p class="category">Apparel</p>
+//     <p class="subcategory">Topwear</p>
+//     <p class="articletype">Tshirts</p>
+//     <p class="season">Summer</p>
+//     <p class="productionyear">2011</p>
+//     <p class="productdisplayname">Sahara Team India Fanwear Round Neck Jersey</p>
+//     <p class="price">895</p>
+//     <p class="discount">null</p>
+//     <p class="brandname">Nike</p>
+//     <p class="soldout">0</p>
+// </article>
+// </template>
